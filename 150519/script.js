a26a89ebba213581f0582b70f568ae1425700521
ShahlaMirzayeva96 <49427424+ShahlaMirzayeva96@users.noreply.github.com>
2019-05-15 23:23:30 +0400

    Add files via upload

---
+++ 150519/script.js
@@ -0,0 +1,11 @@
+//Bir herf sehvine gore alinmirdi.Ona gore gec oldu uzrlu bilin
+//Daha da gec olmasin deye CSS-de cox is gormedim.
+
+
+function openNav(){
+    document.getElementById("mySide").style.width="250px";
+}
+function closeNav(){
+    document.getElementById("mySide").style.width="0px";
+
+}
